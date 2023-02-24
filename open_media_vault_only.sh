24dfb3a09fa14b8d9981708f212f08b678412ace
RomanNum3ral <116513200+RomanNum3ral@users.noreply.github.com>
2023-02-23 22:19:19 -0500

    Update open_media_vault_only.sh

--- open_media_vault_only.sh
+++ open_media_vault_only.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-sudo apt-get update
-sudo dpkg --clear-avail
-sudo apt-get update
-sudo wget -O - https://raw.githubusercontent.com/OpenMediaVault-Plugin-Developers/installScript/master/install | sudo bash
+sudo apt-get update && sudo apt-get upgrade -y
 sudo apt install -y python
 sudo apt install -y pigpio python-pigpio python3-pigpio git
 git clone https://github.com/geekworm-com/x735-v2.5
@@ -11,4 +8,4 @@ sudo chmod +X *.sh
 sudo bash install.sh
 echo "alias off='sudo x735softsd.sh'" >> ~/.bashrc
 echo "python /home/pi/x735-v2.5/pwm_fan_control.py&" >> ~/.bashrc
-sudo reboot now
+sudo wget -O - https://raw.githubusercontent.com/OpenMediaVault-Plugin-Developers/installScript/master/install | sudo bash
